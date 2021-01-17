63456d6b46481b58f265b55eff166ed307417951
Eneko Alonso <eneko.alonso@gmail.com>
2021-01-16 20:26:02 -0800

    Add encoding/decoding JSON strategies (#2)

--- Package.swift
+++ Package.swift
@@ -5,13 +5,16 @@ let package = Package(
     name: "Kebab",
     products: [
         .library(name: "Kebab", targets: ["Kebab"]),
-        .library(name: "KebabExtensions", targets: ["KebabExtensions"])
+        .library(name: "KebabExtensions", targets: ["KebabExtensions"]),
+        .library(name: "KebabJSON", targets: ["KebabJSON"])
     ],
     dependencies: [],
     targets: [
         .target(name: "Kebab", dependencies: []),
         .target(name: "KebabExtensions", dependencies: ["Kebab"]),
+        .target(name: "KebabJSON", dependencies: ["Kebab"]),
         .testTarget(name: "KebabTests", dependencies: ["Kebab"]),
-        .testTarget(name: "KebabExtensionsTests", dependencies: ["KebabExtensions"])
+        .testTarget(name: "KebabExtensionsTests", dependencies: ["KebabExtensions"]),
+        .testTarget(name: "KebabJSONTests", dependencies: ["KebabJSON"])
     ]
 )
